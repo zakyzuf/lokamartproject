026441a9875bbe60c6f17cb1a5514192a0d8de4b
zakyuf <mochammadzakyz@gmail.com>
2023-06-10 14:44:55 +0700

    Update role & add produk

--- database/migrations/2023_05_10_034541_create_orders_table.php
+++ database/migrations/2023_05_10_034541_create_orders_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('order', function (Blueprint $table) {
             $table->id();
             $table->foreignId('id_shipper')->constrained('shipper')->onDelete('restrict')->onUpdate('cascade');
-            $table->foreignId('id_payment')->constrained('payment')->onDelete('restrict')->onUpdate('cascade');
             $table->string('nama_penerima');
             $table->string('notelp_penerima');
             $table->string('catatan_order');
